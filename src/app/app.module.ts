cc56296bccff8755c1e0cb80f65c7a051d7977b5
Shan Desai <itsmeshantanu@hotmail.com>
2017-06-06 16:55:09 +0200

    Add all the Angular files for App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {DashboardComponent} from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { HeroSearchComponent} from './hero-search.component';
+
+import {AppRoutingModule} from './app-routing.module';
+
+// in memory web api
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api/in-memory-web-api.module';
+import {InMemoryDataService} from './in-memory-data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
